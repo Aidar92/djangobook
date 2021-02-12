1423f31a2644a8f51d7a8352c87a3accd35bfe2a
Aidar92 <gumerovaidar@yandex.ru>
2021-02-12 17:19:29 +0300

    chapter 5

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,16 +1,18 @@
 from django import forms
-from django.contrib.auth import models
 from django.contrib.auth.models import User
-from django.forms import fields
 from .models import Profile
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -22,10 +24,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
+
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
